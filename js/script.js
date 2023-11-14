2ccc8cabe53fd26ffe51c9a956ed02b77c91ac20
Arzkana <150640934+Arzkana@users.noreply.github.com>
2023-11-14 12:16:30 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -11,3 +11,10 @@ document.addEventListener("click", function (e) {
     navbarnav.classList.remove("active");
   }
 });
+
+//bar stats
+const spans = document.querySelectorAll(".progress-bar span");
+spans.forEach((span) => {
+  span.style.width = span.dataset.width;
+  span.innerHTML = span.dataset.width;
+});
